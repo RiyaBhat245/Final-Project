0377bf3bf2344bf6b6ec18647f2f52ba0406576a
Riya Bhat <riyabhat@outlook.com>
2019-12-15 09:25:11 -0700

    4th commit of the final project

--- final.js
+++ final.js
@@ -17,9 +17,19 @@ function calculateBmi() {
             if(finalBmi > 18.5 && finalBmi < 25) {
                 document.getElementById("meaning").value = "You are healthy.";
             }
-            if(finalBmi > 25) {
+            if(finalBmi > 25 && finalBmi < 30) {
                 document.getElementById("meaning").value = "You are considered overweight.";
             }
+            if(finalBmi > 30 && finalBmi < 40) {
+                document.getElementById("meaning").value = "You are considered severely obese.";
+            }
+            if(finalBmi > 40 && finalBmi < 45) {
+                document.getElementById("meaning").value = "You are considered morbidly obese.";
+            }
+            if(finalBmi > 45) {
+                document.getElementById("meaning").value = "You are considered super obese.";
+            }
+            
     }
     else {
         alert("Please fill everything in correctly!!");
