6f164ec0c5205d1789dc3a01b97d7b6fb169f815
Riya Bhat <riyabhat@outlook.com>
2019-12-16 20:37:30 -0700

    6th commit of final project, new items added

--- final.js
+++ final.js
@@ -10,26 +10,37 @@ function calculateBmi() {
         var meaningID = document.getElementById("meaning");
         
         document.getElementById("bmi").value = finalBmi.toFixed(2).toString();
-
+ 
             if(finalBmi < 18.5) {
                 document.getElementById("meaning").value = "You are considered underweight.";
+                document.getElementById("bmi").style.color = 'red';
             }
             if(finalBmi > 18.5 && finalBmi < 25) {
                 document.getElementById("meaning").value = "You are healthy.";
+                document.getElementById("bmi").style.color = 'green';
+                document.getElementById("meaning").style.color = 'green';
             }
             if(finalBmi > 25 && finalBmi < 30) {
                 document.getElementById("meaning").value = "You are considered overweight.";
+                document.getElementById("bmi").style.color = 'orange';
+                document.getElementById("meaning").style.color = 'orange';
             }
             if(finalBmi > 30 && finalBmi < 40) {
                 document.getElementById("meaning").value = "You are considered severely obese.";
+                document.getElementById("bmi").style.color = 'red';
+                document.getElementById("meaning").style.color = 'red';
             }
             if(finalBmi > 40 && finalBmi < 45) {
                 document.getElementById("meaning").value = "You are considered morbidly obese.";
+                document.getElementById("bmi").style.color = 'red';
+                document.getElementById("meaning").style.color = 'red';
             }
             if(finalBmi > 45) {
                 document.getElementById("meaning").value = "You are considered super obese.";
+                document.getElementById("bmi").style.color = 'red';
+                document.getElementById("meaning").style.color = 'red';
             }
-            
+            document.getElementById("showResult").style.display ="block";
     }
     else {
         alert("Please fill everything in correctly!!");
